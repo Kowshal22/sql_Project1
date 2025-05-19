33c09dd1eab230590f12aa200b45a103feb75a4a
K.KOWSHAL <158495981+Kowshal22@users.noreply.github.com>
2025-05-19 23:43:41 +0530

    Update SQL_PROJECT1.sql

--- SQL_PROJECT1.sql
+++ SQL_PROJECT1.sql
@@ -15,30 +15,6 @@ CREATE TABLE SALES
 SELECT * FROM SALES
 --DATA CLEANING--
 SELECT COUNT(*) FROM SALES
-WHERE 
-	transactions_id IS NULL
-			OR
-	sale_date IS NULL
-			OR
-	sale_time IS NULL
-			OR
-	customer_id IS NULL
-			OR
-	gender IS NULL
-			OR
-	age IS NULL
-			OR
-	category IS NULL
-			OR
-	quantiy IS NULL
-			OR
-	price_per_unit IS NULL
-			OR
-	cogs IS NULL
-			OR
-	total_sale IS NULL
-
-DELETE FROM SALES
 WHERE
 	transactions_id IS NULL
 			OR
